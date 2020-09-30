7de5e5c6a903ad9a9d61b774c05f83dd6de7d101
johnsondev <johnson.dev298@gmail.com>
2020-09-30 23:26:40 +0700

    updated

--- js/custom.js
+++ js/custom.js
@@ -21,4 +21,17 @@ $(document).ready(function() {
     $(".upload-button").on('click', function() {
         $(".file-upload").click();
     });
+
+
+    $(".brand-logo .slider-items").owlCarousel({
+        items: 8,
+        itemsDesktop: [1024, 4],
+        itemsDesktopSmall: [900, 3],
+        itemsTablet: [600, 2],
+        itemsMobile: [320, 1],
+        navigation: !0,
+        navigationText: ['<a class="flex-prev"></a>', '<a class="flex-next"></a>'],
+        slideSpeed: 500,
+        pagination: !1
+    })
 });
